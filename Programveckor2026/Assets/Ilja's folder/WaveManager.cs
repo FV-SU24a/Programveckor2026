dafa4e3193e5498f9548ce4c0081dd95d4e3e17e
FV-SU24a <felicia.vasquez@elev.ga.lbs.se>
2026-01-08 20:33:33 +0100

    enemy

--- Programveckor2026/Assets/Ilja's folder/WaveManager.cs
+++ Programveckor2026/Assets/Ilja's folder/WaveManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 public class WaveManager : MonoBehaviour
-{
+{ //working on a script thats called enemyspawn that will be called in this script..so this script controls when enemies should spawn jst saying
     public int currentWave = 0;
     public WeaponPoolManager weaponPool;
 
--- Programveckor2026/Assets/Programming/Enemy/EnemyStuff.cs
+++ Programveckor2026/Assets/Programming/Enemy/EnemyStuff.cs
@@ -2,15 +2,76 @@ using UnityEngine;
 
 public class EnemyStuff : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int health;
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private int damage = 10;
+    [SerializeField] private float attackRange = 2f;
+    [SerializeField] private float attackRate = 3f;
+
+    private bool isAlive = true;
+
+    private Transform target;
+    private float attackCooldown;
+
+    private void Start()
     {
-        
+        health = maxHealth;
+        target = GameObject.FindWithTag("Player")?.transform;
+        attackCooldown = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!isAlive || target == null) return;
+
+        //distance to player
+        float distance = Vector2.Distance(transform.position, target.position);
+        //move if outside the attack range
+        if(distance > attackRange) 
+        {
+            MoveTowardsPlayer();
+        }
+        else
+        {
+            //try attacking if cooldown allows
+            if(attackCooldown <= 0)
+            {
+                AttackPlayer();
+                attackCooldown = attackRate;
+            }
+        }
+
+        //reduce attack cool down each frame
+        if (attackCooldown > 0) attackCooldown -= Time.deltaTime;
+
+    }
+
+    private void MoveTowardsPlayer() 
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+        transform.position += (Vector3)(direction * speed * Time.deltaTime);
+    }
+    private void AttackPlayer() 
+    {
+        //place holder
+        Debug.Log($"{gameObject.name} attacks the player for {damage} damage");
+    }
+    private void TakeDamage(int amount)
+    {
+        if (!isAlive) return;
+        health -= amount;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isAlive = false;
+        Debug.Log($"{gameObject.name} died");
+
+        Destroy(gameObject, 1f); //delay if death animation comes
     }
 }
