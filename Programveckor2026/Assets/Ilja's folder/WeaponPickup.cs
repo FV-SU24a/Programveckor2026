e7bdc6c426ed74cd2e0aca0b5043a0abcf6a076c
Febasco <ilja.purgin@elev.ga.lbs.se>
2026-01-13 13:32:34 +0100

    I'm doing random shit

--- Programveckor2026/Assets/Ilja's folder/WeaponPickup.cs
+++ Programveckor2026/Assets/Ilja's folder/WeaponPickup.cs
@@ -2,17 +2,58 @@ using UnityEngine;
 
 public class WeaponPickup : MonoBehaviour
 {
+    [Header("Weapon Data")]
     public Weapon weapon;
+
+    [Header("Pickup Settings")]
+    public KeyCode pickupKey = KeyCode.F;
+
+    private bool playerInRange;
+    private PlayerAttack playerAttack;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(pickupKey))
+        {
+            PickupWeapon();
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        PlayerAttack playerAttack = other.GetComponent<PlayerAttack>();
-        if (playerAttack == null) return;
+        if (!other.CompareTag("Player")) return;
+
+        playerAttack = other.GetComponent<PlayerAttack>();
+
+        if (playerAttack != null)
+        {
+            playerInRange = true;
+            Debug.Log("Player in range of weapon: " + weapon.weaponName);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        playerInRange = false;
+        playerAttack = null;
+    }
+
+    void PickupWeapon()
+    {
+        if (weapon == null)
+        {
+            Debug.LogError("WeaponPickup has NO weapon assigned!");
+            return;
+        }
 
         playerAttack.weaponDamage = weapon.damage;
         playerAttack.attackRange = weapon.range;
 
-        Debug.Log("Picked up: " + weapon.weaponName);
+        Debug.Log("Picked up weapon: " + weapon.weaponName +
+                  " | Damage: " + weapon.damage +
+                  " | Range: " + weapon.range);
 
         Destroy(gameObject);
     }
