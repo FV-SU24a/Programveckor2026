e69d1f881abe7bf64f3c942a31c9b3dbd2787a5a
FV-SU24a <felicia.vasquez@elev.ga.lbs.se>
2026-01-15 22:04:11 +0100

    done for now...

--- Programveckor2026/Assets/Programming/player/camfollow.cs
+++ Programveckor2026/Assets/Programming/player/camfollow.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class camfollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
-    public float yOffset = 1f; 
+    public float yOffset = 1f;
+    public float xOffset = 0f;
     public Transform target; 
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
